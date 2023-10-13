a0f704630ffd74919220409756444e1618dea485
NGV <95875573+ngv-sama@users.noreply.github.com>
2023-10-14 00:05:50 +0530

    Update baba.py

--- baba.py
+++ baba.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import os
